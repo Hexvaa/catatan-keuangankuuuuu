823e9242a78b360dc9d15e254bfdbe4c4811875f
Hexvaa <159183372+Hexvaa@users.noreply.github.com>
2026-08-12 18:06:10 +0700

    Add files via upload

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js"
+
+const SUPABASE_URL = "https://qppqqswrwryxbuymesyl.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwcHFxc3dyd3J5eGJ1eW1lc3lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwMjkzMzIsImV4cCI6MjEwMTYwNTMzMn0.UvupOMRtTLcGoVo6pMjB4_zLgzUDFUz9va7w7v1FO_4"
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
